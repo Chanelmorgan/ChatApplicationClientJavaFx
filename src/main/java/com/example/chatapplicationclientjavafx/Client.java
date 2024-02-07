6d7d3dd26cb98f6768ee684422e3892e57db722f
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-07 20:33:29 +0000

    Finished the client class

--- src/main/java/com/example/chatapplicationclientjavafx/Client.java
+++ src/main/java/com/example/chatapplicationclientjavafx/Client.java
@@ -1,5 +1,7 @@
 package com.example.chatapplicationclientjavafx;
 
+import javafx.scene.layout.VBox;
+
 import java.io.*;
 import java.net.Socket;
 
@@ -21,6 +23,7 @@ public class Client {
         }
     }
 
+    // Method that sends messages to the server
     public void sendMessageToServer(String messageToSend){
         try {
             bufferedWriter.write(messageToSend);
@@ -33,6 +36,32 @@ public class Client {
             System.out.println("Error sending message to server.");
             closeEverything(socket, bufferedReader, bufferedWriter);
         }
+    }
+
+
+    //Method that receives the messages from the servers
+    public void receiveMessageFromServer(VBox vBox){
+        // new thread so that it won't be blocking the whole program
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(socket.isConnected()) {
+                    String messageFromServer = null;
+                    try {
+                        messageFromServer = bufferedReader.readLine();
+                        HelloController.addLabel(messageFromServer, vBox);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        System.out.println("Error receiving message from the server.");
+                        closeEverything(socket, bufferedReader, bufferedWriter);
+                        break;
+                    }
+
+                }
+
+
+            }
+        }).start();
     }
 
     // Method to close all the resources that we are using - prevents null pointer exceptions
