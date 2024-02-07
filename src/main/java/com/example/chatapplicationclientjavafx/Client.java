80ba66f2a618dc049a0e8aa4c56cc66f95c75874
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-07 20:30:08 +0000

    Creating the constructor and send message to server methods in the server class

--- src/main/java/com/example/chatapplicationclientjavafx/Client.java
+++ src/main/java/com/example/chatapplicationclientjavafx/Client.java
@@ -1,4 +1,59 @@
 package com.example.chatapplicationclientjavafx;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Client {
+
+    private  Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    public Client(Socket socket){
+        try {
+            this.socket = socket;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch(IOException e){
+            e.printStackTrace();
+            System.out.println("Error creating client");
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    public void sendMessageToServer(String messageToSend){
+        try {
+            bufferedWriter.write(messageToSend);
+            bufferedWriter.newLine(); // need to send over a new line so it knows the message has ended
+            bufferedWriter.flush(); // manually flushing the buffer because it won't be full
+
+
+        } catch(IOException e){
+            e.printStackTrace();
+            System.out.println("Error sending message to server.");
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    // Method to close all the resources that we are using - prevents null pointer exceptions
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+        try{
+            if(bufferedReader != null){
+                // closes the underlying streams as well
+                bufferedReader.close();
+            }
+            if(bufferedWriter != null){
+                bufferedWriter.close();
+            }
+            if(socket != null){
+                socket.close();
+            }
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
